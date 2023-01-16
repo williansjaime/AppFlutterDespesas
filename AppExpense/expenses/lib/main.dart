bbf3f0ee572292c2733f84fa95408130bb62a3a8
williansjaime <williansjaime@Outlook.com>
2023-01-16 09:00:59 -0300

    Montagem dos cards com despesas

--- AppExpense/expenses/lib/main.dart
+++ AppExpense/expenses/lib/main.dart
@@ -31,6 +31,8 @@ class expenses extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
               width: double.infinity,
@@ -39,9 +41,52 @@ class expenses extends StatelessWidget {
                 child: Text('Grafico'),
                 elevation: 5,
               )),
-          Card(
-            child: Text('Lista de Transações'),
-          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
